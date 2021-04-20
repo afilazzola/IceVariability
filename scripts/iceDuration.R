24c2595fd15affa4d2e1169a465055538829c735
afilazzola <alex.filazzola@outlook.com>
2021-04-20 16:27:26 -0400

    revised the analyses to include percent change from historic patterns. Cleaned up repository to remove some extra files

--- scripts/iceDuration.R
+++ scripts/iceDuration.R
@@ -105,12 +105,102 @@ timeseriesChange <- read.csv("data//durationTimeseries.csv")
 avgTimeseries <- timeseriesChange %>% filter(!(GCM == "gfdl-esm2m" & model=="lake")) %>% ## drop anomalous model
     group_by(Year, RCP) %>% summarize(meanDuration = mean(duration))
 
-currentTimeseries <- avgTimeseries %>% filter(Year <2007) %>% group_by(Year) %>% summarize(meanDuration = mean(meanDuration))
+currentTimeseries <- avgTimeseries %>% filter(Year <2007) %>% group_by(Year) %>% summarize(meanDuration = mean(meanDuration)) %>% data.frame()
 
+## Calculate mean to determine anomaly
+meanSeries <- mean(currentTimeseries[currentTimeseries$Year %in% c(1970:1999), "meanDuration"])
 
-
-ggplot() + theme_classic() + xlim(1901,2099) + ylim(190,250) + 
-  geom_line(data = currentTimeseries, aes(x=Year, y= meanDuration), size=1.3) + 
-  geom_line(data = avgTimeseries %>% filter(Year >2005), aes(x=Year, y= meanDuration, color=RCP ), size=1.3) +
+ggplot() + theme_classic() + xlim(1901,2099) + 
+  geom_line(data = currentTimeseries, aes(x=Year, y= meanDuration-meanSeries), size=1.3) + 
+  geom_line(data = avgTimeseries %>% filter(Year >2005), aes(x=Year, y= meanDuration-meanSeries, color=RCP ), size=1.3) +
   ylab("Mean Lake Ice duration (days)") + 
-  scale_colour_manual(values=c("#F0E442","#E69F00",  "#D55E00"))
+  scale_colour_manual(values=c("#F0E442","#E69F00",  "#D55E00")) +
+  geom_hline(yintercept=0, lty=2)
+
+
+modelsOndropGFDL <- modelsOn[grep("(lake)(?:.+)(gfdl)(?:.+)(rcp85)", modelsOn$filepath, invert=T),]
+
+modelsOffdropGFDL <- modelsOff[grep("(lake)(?:.+)(gfdl)(?:.+)(rcp85)", modelsOff$filepath, invert=T),]
+
+## load current duration to take difference
+currentDiff <- raster( "out//CurrentDurationAll.tif")
+
+## specify cluster
+CL <- makeCluster(3, type="PSOCK")
+clusterExport(cl=CL, list("currentDiff", "modelsOndropGFDL", "modelsOffdropGFDL", "modelsOn"),
+              envir=environment())
+registerDoParallel(cores = CL)
+
+foreach(i = 1:3,  cl=CL, .packages=c("raster","ncdf4")) %dopar%  {
+  
+
+  RCPs <- unique(modelsOn$RCP)
+  
+
+  ## future duration
+  # iceOn <- stack(modelsOn$filepath, bands=70:99) ## current ice on 
+  # iceOff <- stack(modelsOff$filepath, bands=70:99) ## current ice off
+  iceOn <- stack(grep(RCPs[i], modelsOndropGFDL$filepath, value=T), bands=170:199)
+  iceOff <- stack(grep(RCPs[i], modelsOffdropGFDL$filepath, value=T), bands=170:199)
+  durationAll <- iceOff-iceOn
+  meanDuration <- mean(durationAll)
+  
+  ## Diff raster
+  futureDiffDuration <- currentDiff - meanDuration
+  
+  writeRaster(futureDiffDuration, paste0("out//Future",RCPs[i],".tif") )
+
+
+}
+
+## function to make map
+WorldMap <- function(x, plotlabel){
+
+## make plot
+## Convert the dataframe to plot properly
+modelDiffDF <- as(x, "SpatialPixelsDataFrame")
+modelDiffDF <- as.data.frame(modelDiffDF)
+colnames(modelDiffDF) <- c("value", "x", "y")
+
+
+# ## Current
+# world <- map_data("world")
+# ggplot() +  geom_map(data=world, map = world, aes(long, lat, map_id = region), fill = "darkgray") +theme_Publication() +
+#   geom_tile(data= modelDiffDF , aes(x=x, y=y, fill=value)) +
+#   scale_fill_gradient2(  low = "#00008B", mid = "#87CEFA", high="#FFFFFF", midpoint = 250) +
+#   xlab("Longitude") + ylab("Latitude")+
+#   theme(legend.position = c(0.1, 0.3),legend.direction = "vertical", legend.title = element_blank(),legend.key.height = unit(0.5,"cm")) +
+#   xlim(-180,180) + ylim(-90,90) + annotate(geom="text", -170, 90, label = plotlabel, size=6)
+# 
+## Future difference
+world <- map_data("world")
+ggplot() +  geom_map(data=world, map = world, aes(long, lat, map_id = region), fill = "darkgray") +theme_Publication() +
+  geom_tile(data= modelDiffDF , aes(x=x, y=y, fill=value)) +
+  scale_fill_gradient2(  low = "yellow", mid = "orange", high="red", midpoint = 35) +
+  xlab("Longitude") + ylab("Latitude")+
+  theme(legend.position = c(0.1, 0.3),legend.direction = "vertical", legend.title = element_blank(),legend.key.height = unit(0.5,"cm")) +
+  xlim(-180,180) + ylim(-90,90) + annotate(geom="text", -170, 90, label = plotlabel, size=6)
+}
+
+
+listRasters <- list.files("out//", full.names = T)
+
+c0 <- raster(listRasters[1])
+f1 <- raster(listRasters[2])
+f2 <- raster(listRasters[3])
+f3 <- raster(listRasters[4])
+
+# 
+# p1 <- WorldMap(c0, "Current")
+p2 <- WorldMap(f1, "RCP 2.6")
+p3 <- WorldMap(f2, "RCP 6.0")
+p4 <- WorldMap(f3, "RCP 8.5")
+
+gridExtra::grid.arrange(p1, p2, p3, p4, ncol=2)
+
+##  Extract the total area from the raster
+test <- nc_open("data//isimip_lake_info2.nc")
+lakearea = ncvar_get(test, "tot_area")
+
+
+str(lakearea)
---
+++ scripts/iceLoss.r
@@ -0,0 +1,98 @@
+#### Ice duration
+
+## Load packages and functions
+library(raster)
+library(ncdf4)
+library(tidyverse)
+library(ggthemes)
+library(doParallel)
+library(foreach)
+
+source("functions.r")
+
+## Lake Characteristics
+lakearea <- raster("data//isimip_lake_info2.nc", varname="tot_area")
+lakearea <- crop(lakearea, extent(-180, 180, 30, 90))
+
+## List files
+allfiles <- list.files("data//isimip_ice", full.names = T)
+iceOnFiles <- allfiles[grep("icestart", allfiles)]
+iceEndFiles <- allfiles[grep("iceend", allfiles)]
+
+## get model names
+allmodels <- iceOnFiles %>% gsub(".*_ice/", "", .) %>% gsub("_historical.*", "", .)
+GCMmodels <- gsub(".*_", "", allmodels)
+lakemodels <- gsub("_.*", "", allmodels)
+RCPmodels <- iceOnFiles %>% gsub(".*historical_", "", .) %>% gsub("_icestart.*", "", .)
+
+## Convert models to text
+modelsOn <- data.frame(lakemodel = lakemodels, GCM = GCMmodels, RCP = RCPmodels, filepath=iceOnFiles, stringsAsFactors = F)
+
+## get model names
+allmodels <- iceEndFiles %>% gsub(".*_ice/", "", .) %>% gsub("_historical.*", "", .)
+GCMmodels <- gsub(".*_", "", allmodels)
+lakemodels <- gsub("_.*", "", allmodels)
+RCPmodels <- iceEndFiles %>% gsub(".*historical_", "", .) %>% gsub("_iceend.*", "", .) 
+
+## Convert models to text
+modelsOff <- data.frame(lakemodel = lakemodels, GCM = GCMmodels, RCP = RCPmodels, filepath=iceEndFiles, stringsAsFactors = F)
+
+
+## Pull out ice cover for each pair raster
+
+
+durationDiff <- function(model, GCM, RCP){
+  
+  ## Select models
+  modelTemp <- paste0("(",model,")(?:.+)(",GCM,")(?:.+)(",RCP,")")
+  
+  ## calculate duration across timeseries
+  iceOn <- stack(grep(modelTemp, modelsOn$filepath, value=T))
+  iceOff <- stack(grep(modelTemp, modelsOff$filepath, value=T))
+  durationAll <- iceOff-iceOn
+  
+  ## reclassify Ice present/absence
+  icePresence <- reclassify(durationAll, rcl= data.frame(from = c(-999,0), to=c(0,999), becomes=c(0,1)))
+  iceArea <- lakearea * icePresence
+ 
+  ## Get yearly means
+  dfIce <- data.frame(Model= model, GCMs = GCM, RCPs = RCP, Year = 1:199, GlobalIceArea = cellStats(iceArea, sum))
+
+  return(dfIce)
+}
+
+durationDiff("albm","gfdl","rcp26")
+
+
+
+dfIceAll <- lapply(1:46, function(i) {
+  durationDiff(modelsOff[i,"lakemodel"], modelsOff[i, "GCM"], modelsOff[i,"RCP"])
+  
+})
+
+allCombined <- do.call(rbind, dfIceAll)
+
+write.csv(allCombined, "out//iceLossPatterns.csv", row.names=FALSE)
+
+iceLossPatterns <- read.csv("out//iceLossPatterns.csv")
+
+
+meanIceLoss <- iceLossPatterns %>% mutate(Year = Year+1900) %>%  filter(!(Model == "lake" & GCMs == "gfdl-esm2m" & RCPs == "rcp85")) %>% 
+  group_by(RCPs, Year) %>% summarize(iceArea = mean(GlobalIceArea)) %>% data.frame()
+
+
+## averaging from the norm
+iceAreaAverage <- mean(meanIceLoss[meanIceLoss$Year %in% 1970:1999, "iceArea"] )
+
+currentIceloss <- meanIceLoss %>% filter(Year <2007) %>% group_by(Year) %>% 
+  summarize(iceAreamean = mean(iceArea)) %>% mutate(perChange = iceAreamean/iceAreaAverage ) %>% 
+  data.frame()
+futureIceloss <- meanIceLoss %>% filter(Year >2006) %>%  mutate(perChange = iceArea/iceAreaAverage ) 
+
+ggplot() + geom_line(data=currentIceloss, aes(x=Year, y=perChange), size=1.2) +
+  geom_line(data=futureIceloss, aes(x=Year, y=perChange, color=RCPs), size=1.2) +
+  theme_Publication() +  scale_colour_manual(values=c("#F0E442","#E69F00",  "#D55E00")) +
+  ylab("Percent Change in Lake Ice Area") + 
+  geom_hline(yintercept=1, lty=2)
+
+
